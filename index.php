cf75bfe06e2c5dae28a5789b2213aa954687506b
VinceS42 <v.fayon.pro@gmail.com>
2023-06-27 15:34:51 +0200

    Exo1 PHP

---
+++ index.php
@@ -0,0 +1,85 @@
+
+
+
+<div>
+    <?php
+        $lastname = "FAYON";
+        $firstname = "Vincent";
+        $age = 35;
+        $km = 125;
+
+        echo $lastname. $firstname. $age. $km;
+    ?>
+</div>
+
+<div>
+    <?php 
+
+    $string = "Salut";
+    $int = 12;
+    $float = 7.5546;
+    $boolean = TRUE;
+    // $baliseO = "<p>";
+    // $baliseF = "</p>";  
+    
+    // echo $baliseO. $lastname. $baliseF;
+    // echo $baliseO. $firstname. $baliseF;
+    // echo $baliseO. $age. $baliseF;
+    // echo $baliseO. $km. $baliseF;
+    echo "Voici un string : ".$string.", suivis d'un int : ".$int. ", avec un float :".$float." et enfin un booléan : ".$boolean;
+
+    ?>
+</div>
+
+<div>
+    <?php  
+    // $int = null //"rien";
+    $int = 155; 
+    echo "Voici mon int:".$int;    
+    ?>
+</div>
+<div>
+    <?php  
+        $name = 'FAYON Vincent';
+        echo "Bonjour ".$name. ", comment vas-tu? "; 
+    ?>
+</div>
+
+<div>
+    <?php
+        $lastname = "FAYON";
+        $firstname = "Vincent";
+        $age = 35;
+
+        echo "Exercice7: Bonjour " .$lastname. " " .$firstname.", tu as " .$age. "ans.";
+    ?>
+</div>
+
+<div>
+    <?php
+    $variable1 = 3 + 4;
+    $variable2 = 5*20;
+    $variable3 = 45+5;
+    $bOuvrante = "<p>";
+    $bFermante = "</p>";
+
+    echo $bOuvrante;
+    echo "Voila le resultat de la 1ere variable :".$variable1;
+    echo $bFermante;
+
+    echo $bOuvrante;
+    echo "Voila le resultat de la 2eme variable :".$variable2;
+    echo $bFermante;
+
+    echo $bOuvrante;
+    echo "Voila le resultat de la 3eme variable :".$variable3;
+    echo $bFermante;
+
+    ?>
+</div>
+
+<div>
+    <?php
+    
+    ?>
+</div>
